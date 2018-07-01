41df62f0bcef981752b1e2a0f7abbe37d471f5f3
Thomas Kerbrat <kerbrat@outlook.com>
2018-07-01 14:37:21 +0200

    Add a matrix static method

--- utility.js
+++ utility.js
@@ -3,4 +3,18 @@ class Utility {
     static randomInt(max) {
         return Math.floor(Math.random() * max);
     }
+
+    static matrix(width, height, valueInitializerCallback) {
+        const matrix = [];
+        const isFunction = typeof valueInitializerCallback === 'function';
+
+        for (let x = 0; x < width; x++) {
+            matrix[x] = [];
+            for (let y = 0; y < height; y++) {
+                matrix[x][y] = isFunction ? valueInitializerCallback(x, y, width, height) : undefined;
+            }
+        }
+
+        return matrix;
+    }
 }
